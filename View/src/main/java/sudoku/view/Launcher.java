cf8f1485cc21a5ca3a67466550e99adb35d0fc7f
Igor <igoryasha71@gmail.com>
2026-04-07 01:47:39 -0500

    Added Launcher.java

--- View/src/main/java/sudoku/view/Launcher.java
+++ View/src/main/java/sudoku/view/Launcher.java
@@ -2,7 +2,7 @@ package sudoku.view;
 
 public class Launcher {
     public static void main(String[] args) {
-        App.main(args); // Replace 'Main' with your actual main class name
+        App.main(args); 
     }
 }
 
